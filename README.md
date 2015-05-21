6b7c53a6f4be48fe63adad9f95e650389712e4e1
Rizwan Ali <rizcube@gmail.com>
2015-05-21 20:20:48 +0100

    When user make a guess user is given a message whether the guess is correct or incorrect

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ var message = "<p>" + randomNumber + " is a number random number between "  + bo
 document.write(message);
 
 ```
+
+
+Build a Simple Random Number Guessing Game
+
+1. Generate a Random Number
+2. prompt user to guess the number
+3. parseInt the guess
+4. finally and conditional Statement
+
+```
+var randomNumber = Math.floor (Math.random() * 5) + 1;
+var input = prompt ("I am thinking of a number betwee 1 and 6, What is it?");
+var userGuess = parseInt(input);
+document.write (userGuess + 1);
+if (userGuess === randomNumber) {
+  document.write("You guessed the number")
+} else {
+  document.write("Sorry, Your guess is not right")
+}
+
+
+```
--- random.js
+++ random.js
@@ -1,9 +1,10 @@
-var inputTwo = prompt("Please type a starting number");
-bottomNumber = parseInt(inputTwo);
-var inputOne = prompt("Please type an ending  number");
-topNumber = parseInt(inputOne);
+var randomNumber = Math.floor (Math.random() * 5) + 1;
+var input = prompt ("I am thinking of a number betwee 1 and 6, What is it?");
+var userGuess = parseInt(input);
+document.write (userGuess + 1);
+if (userGuess === randomNumber) {
+  document.write("You guessed the number")
+} else {
+  document.write("Sorry, Your guess is not right")
+}
 
-randomNumber = (Math.floor(Math.random() * (topNumber - bottomNumber +1)) + bottomNumber);
-
-var message = "<p>" + randomNumber + " is a number random number between "  + bottomNumber +  " and "  + topNumber + "</p>"
-document.write(message);
