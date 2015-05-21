09ffec92eddee6740d470a368553d099b5bf45f8
Rizwan Ali <rizcube@gmail.com>
2015-05-21 21:30:13 +0100

    added condition for the guess being greater and lower

--- random.js
+++ random.js
@@ -1,10 +1,25 @@
-var randomNumber = Math.floor (Math.random() * 5) + 1;
-var input = prompt ("I am thinking of a number betwee 1 and 6, What is it?");
-var userGuess = parseInt(input);
-document.write (userGuess + 1);
-if (userGuess === randomNumber) {
-  document.write("You guessed the number")
-} else {
-  document.write("Sorry, Your guess is not right")
+var correctGuess = false;
+var randomNumber = Math.floor (Math.random() * 6) + 1;
+var guess = prompt ("I am thinking of a number between 1 and 6, What is it?");
+
+if (parseInt(guess) === randomNumber) {
+  correctGuess = true;
+
+} else if (parseInt(guess) < randomNumber) {
+  var guessMore = prompt("Try higher number");
+    if (parseInt(guessMore) === randomNumber) {
+    correctGuess = true;
+    }
+
+} else if (parseInt(guess) > randomNumber) {
+  var guessless = prompt("Try lower number");
+    if (parseInt(guessless) === randomNumber) {
+      correctGuess = true;
+    }
 }
 
+if (correctGuess === true) {
+  document.write("Well done! You Guessed the number: " + guess )
+} else {
+  document.write("Sorry! the number was: " + randomNumber)
+}
